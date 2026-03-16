c56f9230e9ac168d070b3ce4eb5318c39ed92358
setday <32331578+setday@users.noreply.github.com>
2026-03-16 18:05:33 +0300

    Add diffgrm model

---
+++ src/models/abstract_model.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+
+class AbstractModel(nn.Module):
+    def __init__(
+        self,
+        config: dict,
+    ):
+        super(AbstractModel, self).__init__()
+
+        self.config = config
+
+    @property
+    def n_parameters(self):
+        total_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        return f'Total number of trainable parameters: {total_params}'
+
+    def calculate_loss(self, batch):
+        raise NotImplementedError('calculate_loss method must be implemented.')
+
+    def generate(self, batch, n_return_sequences=1):
+        raise NotImplementedError('predict method must be implemented.')
